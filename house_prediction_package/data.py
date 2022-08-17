86346bb6ff775846a8fa4cf3745e94a5ce5ced77
pierre beylard <pierre.beylard@hotmail.fr>
2022-08-17 20:25:40 +0200

    adding code to upload data in db

--- house_prediction_package/data.py
+++ house_prediction_package/data.py
@@ -1,4 +1,6 @@
 import pandas as pd
+import requests
+from sqlalchemy import create_engine
 
 class GetData:
     """ Read data from csv and load it in a dataframe
@@ -31,5 +33,56 @@ class GetData:
     def enrichissement_insee(self,df):
         pass
 
-    def loading_data_db (self,df):
-        pass
+
+class loading_data_in_db:
+    """ class helping to load data into a database
+    please enter db name as second argument
+    please enter table name as third argument"""
+
+    def __init__(self, df, db_name, table_name):
+        self.df = df
+        self.db_name = db_name
+        self.table_name = table_name
+
+    def load_df_db(self):
+        engine = create_engine(f'sqlite:///../data/{self.db_name}.sqlite',
+                               echo=True)  # pass your db url
+        self.df.to_sql(name=self.table_name,
+                       con=engine,
+                       if_exists='replace',
+                       index=False)
+
+
+class api_enrichment :
+    """Objective : obtain latitude, longitude from 2 apis :
+    https://adresse.data.gouv.fr/api-doc/adresse --- to normalize & obtain coordinates from an address
+    https://pyris.datajazz.io/ --for IRIS
+    """
+
+    def __init__(self,
+                 addresse):
+        self.addresse = addresse
+
+    def enrichissement_coordinates(self):
+        try :
+            long = requests.get(
+                f'https://api-adresse.data.gouv.fr/search/?q={self.addresse}').json(
+                )['features'][0]['geometry']['coordinates'][0]
+            lat = requests.get(
+                f'https://api-adresse.data.gouv.fr/search/?q={addresse_for_enrichment}').json(
+                )['features'][0]['geometry']['coordinates'][1]
+        except :
+            long = 'not found'
+            lat = 'not found'
+        return self
+
+    def enrichissement_iris(self):
+        coordinates = (str(self.lat), str(self.long))
+        coordinates = "&lon=".join(coordinates)
+        pyris_api_url = "https://pyris.datajazz.io/api/coords?geojson=false&lat="
+        try:
+            self.IRIS = requests.get(
+                f'{pyris_api_url}{coordinates}').json()['complete_code']
+        except:
+            self.IRIS = 'not found'
+        return self.IRIS
